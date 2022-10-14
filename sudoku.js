08c2ddecd96afbf6ecb64decb32b2e3d016ce4be
Vanya <geo.vox95@gmail.com>
2022-10-14 16:52:50 +0300

    test sudoku

--- sudoku.js
+++ sudoku.js
@@ -6,15 +6,57 @@
 function solve(boardString) {
   const board = [];
   let j = 9
+  //Создаем из строки правильный массив
   for (let i = 0; i < boardString.length; i += 9) {
     const arrIn = boardString.slice(i, j).split('').map(el => isNaN(el) ? el : +el);
     board.push(arrIn);
     j += 9;
   }
-  return board
+  // Решение 
+      // Проверяем правильность заполненных чисел строка строка столбец столбец номер заполненные числа
+  let isValid = (row,col,num) => {
+    // Цикл пытаемся заполнить числа 
+    for (let i = 0; i < 9; i++) {
+        let boxRow = (parseInt(row / 3) * 3) + parseInt(i / 3); // Маркер суб-судоку
+        let boxCol = (parseInt(col / 3) * 3) + i % 3;// Подпись столбца Sub Sudoku
+        //console.log(boxRow + '_' + boxCol); 
+        if (board[row][i] == num ||  board[i][col] == num ||  board[boxRow][boxCol] == num) {
+          return false;
+        }
+    }
+    return true;
+  }
+
+let solve =() => {
+  // Цикл
+  for (let i = 0; i < 9; i++) {
+  // Зациклить столбец
+  for (let j = 0; j < 9; j++) {
+          if (board[i][j] == '-') {
+              for(let num = 1; num<10; num++) {
+                // Проверяем правильность заполненных чисел
+                if (isValid(i,j,num)) {
+                  board[i][j] = num;
+                  // Рекурсия
+                  if (solve(board)) {
+                    return true;
+                  }
+                  board[i][j] = '-';
+                }
+              }
+              return false;
+          }
+  }
 }
-const boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
-console.log(solve(boardString));
+return true;
+}
+solve(board);
+return board
+}
+// const boardString = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+
+// console.table(solve(boardString));
+
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
